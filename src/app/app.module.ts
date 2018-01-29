e0c57cb367b8a6dbf97391c3ed14a92f42d9d8be
Augusto <augustomesquitasrs@gmail.com>
2018-01-29 16:37:45 -0200

    Removido import de bootstrap de app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
 import { AppComponent } from './app.component';
-import { } from 'bootstrap';
 import { ComponenteReusavelComponent } from './componente-reusavel/componente-reusavel.component';
 
 @NgModule({
